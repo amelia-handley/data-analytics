87b5cf1d65fe5d2ee7cb2770018460124255456b
amelia-handley <40326169@live.napier.ac.uk>
2019-10-16 18:11:33 +0100

    Found 3 possible relationships

--- data-analytics-cw1/cw1.R
+++ data-analytics-cw1/cw1.R
@@ -23,6 +23,22 @@ ggplot(dataset, aes(x=Age)) + geom_histogram(bins=30)
 # Histogram: Part.B.Score - Outlier 2
 ggplot(dataset, aes(x=Part.B.Score)) + geom_histogram(bins=100)
 
+# Graph 1: Age,Band and Part.C.Score(~Location)
+ggplot(dataset, aes(x=Age.Band, y=Part.C.Score)) + geom_bar(stat="Identity") + facet_wrap(~Location)
+# Graph 2: Age.Band and Completion.Time (~Gender)
+ggplot(dataset, aes(x=Age.Band, y=Completion.Time)) +geom_boxplot() + facet_wrap(~Gender)
+# Graph 3: Part.C.Score (~Location) ???
+ggplot(dataset, aes(x=Part.C.Score)) + geom_histogram(bins=50) + facet_wrap(~Location)
+# Graph 4: Part.A.Score (~Age.Band) ???
+ggplot(dataset, aes(x=Part.A.Score)) + geom_histogram(bins=30) + facet_wrap(~Age.Band)
+# Graph 5
 
-######### UNKNOWN ##########
+
+######### POSSIBLE ##########
 ggplot(dataset, aes(x=Location, y=Age)) + geom_boxplot()
+
+ggplot(dataset, aes(x=Completion.Time, y=Part.C.Score)) + geom_point() + facet_wrap(~Age.Band)
+
+ggplot(dataset, aes(x=Location, y=Part.B.Score)) + geom_bar(stat="Identity") + facet_wrap(~Age.Band)
+
+ggplot(dataset, aes(x=Location, y=Part.C.Score)) + geom_boxplot() + scale_y_log10()
